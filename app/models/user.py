075775e7ecd851629b98c96b3b32367baaf9eeb6
akashrajeshnair <nairakash2004@gmail.com>
2024-12-04 20:43:01 +0530

    minor fix

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,6 @@ from datetime import datetime
 class User(BaseModel):
     firebase_uid: str
     email: EmailStr
-    username: str
     languages: List[str] = []
     created_at: datetime = datetime.now()
     last_login: Optional[datetime] = None
